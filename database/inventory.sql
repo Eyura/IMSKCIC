fe9a81424ac0f0f793a0988b45fe1ab53bb196f1
Eyura <Yudhistiraegi08@gmail.com>
2024-10-15 15:36:02 +0700

    update inventory.sql

--- database/inventory.sql
+++ database/inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2024 at 10:54 AM
+-- Generation Time: Oct 15, 2024 at 10:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
